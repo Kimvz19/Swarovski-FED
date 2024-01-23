3b0cd4d316bf23da95f907b48269e231ce11a768
Kimvz19 <150820324+Kimvz19@users.noreply.github.com>
2024-01-23 09:29:37 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
 // JavaScript Document
 console.log("hi");
+
+var mouseclick = new Audio();
+mouseclick.src = "https://uploads.sitepoint.com/wp-content/uploads/2023/06/1687569402mixkit-fast-double-click-on-mouse-275.wav";
+
+/** bronvermelding https://www.sitepoint.com/button-micro-interactions/ */
